7c950c5099297a69d199fe3ff57796ecc68998c1
vladShadow <vladivashchuk@gmail.com>
2020-04-12 17:08:22 +0300

    Lab2.2 additional task

--- MainActivity.java
+++ MainActivity.java
@@ -6,6 +6,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import com.jjoe64.graphview.GraphView;
 import com.jjoe64.graphview.series.DataPoint;
 import com.jjoe64.graphview.series.LineGraphSeries;
+import com.jjoe64.graphview.series.BarGraphSeries;
 import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
@@ -22,6 +23,8 @@ public class MainActivity extends AppCompatActivity {
     private double notRealKoef2;
     private Double[][] cosMemo = new Double[N][N];
     private Double[][] sinMemo = new Double[N][N];
+    private double timeDFT;
+    private double timeFFT;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,6 +39,14 @@ public class MainActivity extends AppCompatActivity {
         LineGraphSeries<DataPoint> line3 = new LineGraphSeries<>(calculateFastFourie());
         line3.setColor(Color.RED);
 
+        BarGraphSeries<DataPoint> line4 =
+                new BarGraphSeries<>(new DataPoint[] {new DataPoint(10, timeDFT)});
+        line4.setColor(Color.GREEN);
+
+        BarGraphSeries<DataPoint> line5 =
+                new BarGraphSeries<>(new DataPoint[] {new DataPoint(20, timeFFT)});
+        line5.setColor(Color.RED);
+
         GraphView graph = findViewById(R.id.graph1);
         setUpGraph(graph, line1, -5, 6);
 
@@ -43,7 +54,10 @@ public class MainActivity extends AppCompatActivity {
         setUpGraph(graph, line2, 0, 80);
 
         graph = findViewById(R.id.graph3);
-        setUpGraph(graph, line3, 0, 70);
+        setUpGraph(graph, line3, 0, 80);
+
+        graph = findViewById(R.id.graph4);
+        setUpTimeGraph(graph, line4, line5,0, (int)(timeDFT * 1.5));
     }
 
     private DataPoint[] generateRandomSignal(double[] res) {
@@ -76,7 +90,19 @@ public class MainActivity extends AppCompatActivity {
         graph.addSeries(line);
     }
 
+    private void setUpTimeGraph(GraphView graph, BarGraphSeries line, BarGraphSeries line2, int miny, int maxy) {
+        graph.getViewport().setXAxisBoundsManual(true);
+        graph.getViewport().setYAxisBoundsManual(true);
+        graph.getViewport().setMinX(0);
+        graph.getViewport().setMaxX(30);
+        graph.getViewport().setMaxY(maxy);
+        graph.getViewport().setMinY(miny);
+        graph.addSeries(line);
+        graph.addSeries(line2);
+    }
+
     private DataPoint[] calculateFourie() {
+        long start = System.nanoTime();
         DataPoint[] points = new DataPoint[N - 1];
         for (int i = 0; i < N - 1; i++) {
             realKoef = 0;
@@ -93,10 +119,13 @@ public class MainActivity extends AppCompatActivity {
             }
             points[i] = new DataPoint(i, Math.sqrt(Math.pow(realKoef, 2) + Math.pow(notRealKoef, 2)));
         }
+        long end = System.nanoTime();
+        timeDFT = end - start;
         return points;
     }
 
     private DataPoint[] calculateFastFourie() {
+        long start = System.nanoTime();
         DataPoint[] points = new DataPoint[N];
         for (int p = 0; p < N; p++) {
             double temp = 4 * Math.PI * p / N;
@@ -119,6 +148,8 @@ public class MainActivity extends AppCompatActivity {
                         + Math.pow((notRealKoef2 - notRealKoef1 * Math.sin(temp)), 2)));
             }
         }
+        long end = System.nanoTime();
+        timeFFT = end - start;
         return points;
     }
 
